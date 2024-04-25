4f344d35d39bcc1491a00e2294fbadc34761bafa
Jozijozi <josiaalmeida2003@gmail>
2024-04-24 22:02:57 -0300

    Restaurante-Adonis

---
+++ start/routes.ts
@@ -0,0 +1,24 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+import ProdutosController from '../app/controllers/produtos_controller.js'
+
+router.get('/', async () => {
+  return {
+    hello: 'world',
+  }
+})
+router.get('/nome', async () => {
+  return {
+    nome: 'Joziane',
+  }
+})
+
+router.resource('/produtos', ProdutosController).apiOnly()
